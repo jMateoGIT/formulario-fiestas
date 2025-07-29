05999dd7c5191a7c0a3a21c2497becf62e544d6f
jMateoGIT <jesus94_zgz@hotmail.com>
2025-07-29 12:51:56 +0200

    maxdatev2

--- main.js
+++ main.js
@@ -16,7 +16,8 @@
       dateFormat: "d/m/Y",
       locale: flatpickr.l10ns.es,
       allowInput: true,
-      conjunction: ", "
+      conjunction: ", ",
+      maxDate: "31/01/2026"
     });
   };
 
